5c7c55d11b190d65aadd013c4a9598841b960c27
ChechareSuraj0707 <svchechare@gmail.com>
2025-10-04 13:22:34 +0530

    Session 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 {/* <div id="parent">
     <div id="child">
         <h1>I am h1 tag</h1>
@@ -12,7 +15,7 @@
 
 const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")]
+        [React.createElement("h1", {}, "Namaste React"),React.createElement("h2", {}, "I am h2 tag")]
     ),
     React.createElement("div", { id: "child2" },
         [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")])
