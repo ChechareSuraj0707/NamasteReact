4e074b686d41fdfc177a7431b1024fc1450f1ad6
admin <admin@admins-Mac-mini.example.com>
2025-12-04 11:37:23 +0530

    eppo 11

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,10 +4,10 @@ const RestaurantCard = (props) => {
   const { resData } = props;
   const { image, name, rating, cuisine, offer, time } = resData;
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="m-4 p-4 w-[250px] bg-gray-100 rounded-lg hover:bg-gray-200">
       <img className="res-logo" src={image} alt="restaurant" />
       {/* <img className="res-logo" src={cloudinaryImageId ? CDN_URL + cloudinaryImageId : "https://via.placeholder.com/508x320?text=No+Image"} alt="restaurant" /> */}
-      <h3 className="font-bold py-4 text-lg">{name}</h3>
+      <h3 className="font-bold py-2 text-lg">{name}</h3>
       <h4>{cuisine}</h4>
       <h4>{rating} stars</h4>
       <h4>{offer}</h4>
